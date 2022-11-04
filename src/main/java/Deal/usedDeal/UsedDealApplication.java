ff23900ac75a124e75fb16bc423c31d8927322b0
sss_jinn <100845256+Jorados@users.noreply.github.com>
2022-11-04 14:44:51 +0900

    깃 테스트

--- src/main/java/Deal/usedDeal/UsedDealApplication.java
+++ src/main/java/Deal/usedDeal/UsedDealApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class UsedDealApplication {
 
+	//git test test
 	public static void main(String[] args) {
 		SpringApplication.run(UsedDealApplication.class, args);
 	}
